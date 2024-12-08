57b632a18dfb9f60c3db7d49406eee8f2f9ec407
Luciano Ganzero <lucianoganzero94@gmail.com>
2024-12-08 13:14:06 -0300

    CRUD colores

--- app/controllers/colors_controller.rb
+++ app/controllers/colors_controller.rb
@@ -25,7 +25,7 @@ class ColorsController < ApplicationController
 
     respond_to do |format|
       if @color.save
-        format.html { redirect_to @color, notice: "Color was successfully created." }
+        format.html { redirect_to store_management_path, notice: "El color se creó correctamente." }
         format.json { render :show, status: :created, location: @color }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ColorsController < ApplicationController
   def update
     respond_to do |format|
       if @color.update(color_params)
-        format.html { redirect_to @color, notice: "Color was successfully updated." }
+        format.html { redirect_to store_management_path, notice: "El color se actualizó correctamente." }
         format.json { render :show, status: :ok, location: @color }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class ColorsController < ApplicationController
     @color.destroy!
 
     respond_to do |format|
-      format.html { redirect_to colors_path, status: :see_other, notice: "Color was successfully destroyed." }
+      format.html { redirect_to store_management_path, status: :see_other, notice: "El color fue correctamente eliminado." }
       format.json { head :no_content }
     end
   end
--- app/models/color.rb
+++ app/models/color.rb
@@ -1,3 +1,5 @@
 class Color < ApplicationRecord
   has_and_belongs_to_many :products
+
+  validates :code, presence: true, format: { with: /\A#(?:[0-9a-fA-F]{6}|[0-9a-fA-F]{8})\z/, message: "Debe ser un código de color hexadecimal válido (ej. #RRGGBB o #RRGGBBAA)" }
 end
